e478d4e5ba87fb34d13db4610c23966d13aefefc
João Victor <jbatista@lliege.com.br>
2019-08-05 17:29:52 -0300

    Create item

--- app/controllers/produtos.js
+++ app/controllers/produtos.js
@@ -1,8 +1,6 @@
 const fs = require('fs');
 const csv = require('csvtojson');
-const {
-    Parser
-} = require('json2csv');
+const csvWriter = require('csv-write-stream');
 
 const {
     join
@@ -13,23 +11,57 @@ const csvFileName = join(__dirname, '../../data/products.csv');
 module.exports = {
 
     getProdutos: (req, res) => {
-        csv()
-            .fromFile(csvFileName)
-            .then((jsonObj) => {
-                res.status(200).send(jsonObj);
-            });
+        try {
+            csv()
+                .fromFile(csvFileName)
+                .then((jsonObj) => {
+                    res.status(200).send(jsonObj);
+                });
+        } catch (e) {
+            throw e;
+        }
     },
 
     getProdutoByID: (req, res) => {
-        csv()
-            .fromFile(csvFileName)
-            .then((jsonObj) => {
-                for (i = 0; i < jsonObj.length; i++) {
-                    if (jsonObj[i].ID === req.params.id) {
-                        return res.status(200).send(jsonObj[i]);
+        try {
+            csv()
+                .fromFile(csvFileName)
+                .then((jsonObj) => {
+                    for (i = 0; i < jsonObj.length; i++) {
+                        if (jsonObj[i].ID === req.params.id) {
+                            return res.status(200).send(jsonObj[i]);
+                        }
                     }
-                }
-                res.status(404).send("ID Not Found");
-            })
+                    res.status(404).send("ID Not Found");
+                })
+        } catch (e) {
+            throw e;
+        }
     },
+
+    createProduto: (req, res) => {
+        try {
+            let writer = csvWriter({sendHeaders: false});
+            let inputCsv = {};
+            csv()
+                .fromFile(csvFileName)
+                .then((jsonObj) => {
+                    inputCsv = {
+                        id: jsonObj.length + 1,
+                        nome: req.body.nome,
+                        preco: req.body.preco,
+                    }
+                    writer.pipe(fs.createWriteStream(csvFileName, {
+                        flags: 'a'
+                    }))
+                    writer.write(inputCsv, {
+                        sendHeaders: false
+                    })
+                    writer.end()
+                    res.status(201).send('Produto criado');
+                });
+        } catch (e) {
+            throw e;
+        }
+    }
 };
